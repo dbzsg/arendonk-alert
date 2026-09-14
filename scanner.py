fe7bcc6061f653e357349870316891047dcc3a81
dbzsg <siemgevers@gmail.com>
2026-09-14 22:28:12 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -786,11 +786,17 @@ def main():
 
     properties = scan_immo_drie()
 
-    print()
+print()
 
-    process_properties(
-        properties
-    )
+domestic_properties = scan_domestic()
+
+print()
+
+all_properties = properties + domestic_properties
+
+process_properties(
+    all_properties
+)
 
     print()
     print(
