8ff22cbbe00852fc81064ad0519cf6d34b226b44
dbzsg <siemgevers@gmail.com>
2026-09-14 19:06:25 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,2 +1,33 @@
+import os
+import requests
+
+
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+
+def send_telegram(message):
+
+    url = (
+        f"https://api.telegram.org/"
+        f"bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    )
+
+    response = requests.post(
+        url,
+        data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": message
+        },
+        timeout=30
+    )
+
+    response.raise_for_status()
+
+
 print("Vastgoed scanner gestart!")
-print("Telegram koppeling komt hierna.")
+
+send_telegram(
+    "🏠 Vastgoed scanner werkt!\n\n"
+    "GitHub → Python → Telegram ✅"
+)
